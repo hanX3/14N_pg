264270b9fc45d374de240974682f74a48fe05d6c
hanX3 <hanxingchi@gmail.com>
2022-10-13 09:35:50 +0800

    add root macro

---
+++ src/run_macros/macro.cpp
@@ -0,0 +1,33 @@
+
+{
+  gROOT->ProcessLine(".L generate_macro.cpp");
+
+  gROOT->ProcessLine("generate_macro(20)");
+  gROOT->ProcessLine("generate_macro(21)");
+  gROOT->ProcessLine("generate_macro(22)");
+  gROOT->ProcessLine("generate_macro(23)");
+  gROOT->ProcessLine("generate_macro(24)");
+  gROOT->ProcessLine("generate_macro(25)");
+  gROOT->ProcessLine("generate_macro(26)");
+  gROOT->ProcessLine("generate_macro(27)");
+  gROOT->ProcessLine("generate_macro(28)");
+  gROOT->ProcessLine("generate_macro(29)");
+
+  gROOT->ProcessLine("generate_macro(30)");
+  gROOT->ProcessLine("generate_macro(31)");
+  gROOT->ProcessLine("generate_macro(32)");
+  gROOT->ProcessLine("generate_macro(33)");
+  gROOT->ProcessLine("generate_macro(34)");
+  gROOT->ProcessLine("generate_macro(35)");
+  gROOT->ProcessLine("generate_macro(36)");
+  gROOT->ProcessLine("generate_macro(37)");
+  gROOT->ProcessLine("generate_macro(38)");
+  gROOT->ProcessLine("generate_macro(39)");
+
+  gROOT->ProcessLine("generate_macro(40)");
+  gROOT->ProcessLine("generate_macro(41)");
+  gROOT->ProcessLine("generate_macro(42)");
+  gROOT->ProcessLine("generate_macro(24)");
+
+  gROOT->ProcessLine(".q");
+}
