129bfc79053655ca888f52cd9fa1eae7a19a01b0
hanX3 <hanxingchi@gmail.com>
2023-05-03 11:22:13 +0800

    14N_pg_JUNA_only_gamma

---
+++ sim/14N_pg_JUNA_only_gamma/include/Constants.hh
@@ -0,0 +1,90 @@
+#ifndef Constants_h
+#define Constants_h 1
+
+#include "globals.hh"
+#include "G4SystemOfUnits.hh"
+
+
+constexpr G4double worldSizeX = 2. *m;
+constexpr G4double worldSizeY = 2. *m;
+constexpr G4double worldSizeZ = 2. *m;
+
+//beam tube
+constexpr G4double BeamTubeR = 10. * mm;
+constexpr G4double BeamTubeLength = 50. * mm;
+
+//target
+constexpr G4double TargetR = 20. *mm;
+constexpr G4double TargetThickness = 4. *mm;
+
+//target back
+constexpr G4double TargetBackR = 20. *mm;
+constexpr G4double TargetBackThickness = 1. *mm;
+constexpr G4double TargetBackPosDis = TargetThickness + TargetBackThickness;
+
+//flange
+constexpr G4double FlangeR = 82.5 *mm;
+constexpr G4double FlangeRCut = 36. *mm;
+constexpr G4double FlangeThickness = 6.5 *mm;
+constexpr G4double FlangeThicknessCut = 5.25 *mm;
+constexpr G4double FlangePosDis = FlangeThickness + TargetThickness + 2*TargetBackThickness;
+
+//cooling support
+constexpr G4double CoolingSupportR = 36. *mm;
+constexpr G4double CoolingSupportRCut = 20. *mm;
+constexpr G4double CoolingSupportThickness = 4.25 *mm;
+constexpr G4double CoolingSupportThicknessCut = 3.25 *mm;
+constexpr G4double CoolingSupportPosDis = TargetThickness + 2.*TargetBackThickness + 2.*(FlangeThickness-FlangeThicknessCut) + CoolingSupportThickness;
+
+//Ge
+constexpr G4double TargetGeDis = 140. * mm;
+constexpr G4double GeR  = 46.7 * mm;
+constexpr G4double GeHeight  = 54.5 * mm;
+constexpr G4double GePosDis = GeHeight + TargetGeDis + TargetThickness + 2.*TargetBackThickness;
+constexpr G4int NofGe = 1;
+constexpr G4double GeThetaAngle[NofGe] = {0.};
+constexpr G4double LN2HoleR = 5.55 * mm;
+constexpr G4double LN2HoleDeep = 47.95 * mm;
+
+//face1
+constexpr G4double Face1R = 49.7 *mm;
+constexpr G4double Face1Thickness = 0.015 *mm;
+
+//face2
+constexpr G4double Face2R = 55. *mm;
+constexpr G4double Face2Thickness = 0.38 *mm;
+
+//distance
+constexpr G4double Face1Face2Dis = 5. *mm;
+constexpr G4double Face1Dis = GePosDis - GeHeight - Face1Thickness;
+constexpr G4double Face2Dis = GePosDis - GeHeight - 2.*Face1Thickness - Face1Face2Dis - Face2Thickness;
+
+//surround1
+constexpr G4double Surround1Thickness = 0.8 *mm;
+constexpr G4double Surround1R1 = Face1R;
+constexpr G4double Surround1R2 = Surround1R1 + Surround1Thickness;
+constexpr G4double Surround1Length = 60. *mm;
+constexpr G4double Surround1Dis = GePosDis - GeHeight + Surround1Length - 2.*Face1Thickness;
+
+//surround2
+constexpr G4double Surround2Thickness = 1.5 *mm;
+constexpr G4double Surround2R1 = Face2R;
+constexpr G4double Surround2R2 = Surround2R1 + Surround2Thickness;
+constexpr G4double Surround2Length = 75. *mm;
+constexpr G4double Surround2Dis = GePosDis - GeHeight + Surround2Length - 2.*Face1Thickness - Face1Face2Dis - 2.*Face2Thickness;
+
+//beam
+constexpr G4double BeamAngle = 0.;
+constexpr G4double BeamSize = 5 *mm;//r
+constexpr G4double ParticleEnergy = 5181. *keV;
+
+//resolution
+constexpr G4double Energy1 = 1764.5;
+constexpr G4double Sigma1 = 1.276;
+constexpr G4double Energy2 = 2204.2;
+constexpr G4double Sigma2 = 1.41;
+
+//step limit
+constexpr G4double MaxStepLimit = 10 *um;
+
+#endif
---
+++ sim/14N_pg_JUNA_only_gamma/src/PhysicsList.cc
@@ -0,0 +1,61 @@
+#include "PhysicsList.hh"
+
+#include "G4DecayPhysics.hh"
+#include "G4RadioactiveDecayPhysics.hh"
+#include "G4EmStandardPhysics.hh"
+
+#include "G4HadronicProcess.hh"
+#include "G4ProtonInelasticProcess.hh"
+
+#include "G4StepLimiterPhysics.hh"
+
+
+#include "G4ProcessManager.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+PhysicsList::PhysicsList()
+: G4VModularPhysicsList(),
+  electro_magnetic(nullptr),
+  decay(nullptr),
+  radioactive_decay(nullptr)
+{
+  G4int verb = 0;
+  SetVerboseLevel(verb);
+
+  // EM physics
+  electro_magnetic = new G4EmStandardPhysics(verb);
+  RegisterPhysics(electro_magnetic);
+  // Default
+  decay = new G4DecayPhysics(verb);
+  RegisterPhysics(decay);
+  // Radioactive decay
+  radioactive_decay = new G4RadioactiveDecayPhysics(verb);
+  RegisterPhysics(radioactive_decay);
+
+  // step limit
+  stepLimit = new G4StepLimiterPhysics(verb);
+  // RegisterPhysics(stepLimit);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+PhysicsList::~PhysicsList()
+{
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void PhysicsList::ConstructProcess()
+{
+  AddTransportation();
+
+  electro_magnetic->ConstructProcess();
+  decay->ConstructProcess();
+  radioactive_decay->ConstructProcess();
+  // stepLimit->ConstructProcess();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void PhysicsList::SetCuts()
+{
+  G4VUserPhysicsList::SetCuts();
+}
