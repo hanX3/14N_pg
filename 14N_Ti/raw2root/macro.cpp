cc2cd214c2515b293be5bad55c689914ccfa9e31
hanX3 <hanxingchi@gmail.com>
2023-05-03 11:20:23 +0800

    raw2root

---
+++ 14N_Ti/raw2root/macro.cpp
@@ -0,0 +1,23 @@
+{
+  gROOT->ProcessLine(".L raw2root.cpp");
+  gROOT->ProcessLine("raw2root(\"target1\", \"275\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"276\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"277\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"278\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"279\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"280\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"281\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"282\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"283\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"284\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"285\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"286\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"288\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"290\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"292\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"294\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"296\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"298\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"299\")");
+  gROOT->ProcessLine("raw2root(\"target1\", \"300\")");
+}
---
+++ 14N_Ti/raw2root/raw2root.cpp
@@ -0,0 +1,89 @@
+
+void raw2root(TString target, TString energy)
+{
+  cout << "ana " << target << " " << energy << "keV" << endl;
+
+  ifstream fi;
+  fi.open(TString::Format("../../../data/14N_Ti/%s/14N_%skeV.txt", target.Data(), energy.Data()).Data());
+  if(!fi){
+    cout << "can not open file" << endl;
+    return ;
+  }
+
+  char line[256];
+  TString line_new;
+  Double_t real_time, live_time;
+
+  TTree *tr = new TTree("tr", "time info");
+  tr->Branch("real_time", &real_time, "real_time/D");
+  tr->Branch("live_time", &live_time, "live_time/D");
+
+  //
+  while(1){
+    fi.getline(line, 255);
+    // cout << line << endl;
+
+    line_new = TString::Format("%s", line);
+
+    if(line_new.Contains("Channel, Counts")){
+      break;
+    }
+
+    //
+    if(line_new.Contains("Real Time")){
+      int a = line_new.Last(',');
+      int b = line_new.Length();
+
+      TString real_time_str = line_new(a+1, b);
+      real_time = real_time_str.Atof();
+    }
+
+    if(line_new.Contains("Live Time")){
+      int a = line_new.Last(',');
+      int b = line_new.Length();
+
+      TString live_time_str = line_new(a+1, b);
+      live_time = live_time_str.Atof();
+    }
+  }
+
+  cout << "real_time " << real_time << endl;
+  cout << "live_time " << live_time << endl;
+  tr->Fill();
+
+  //
+  TH1D *h = new TH1D("h", "", 16384, 0, 16384);
+  Int_t i, c;
+  while(1){
+    fi.getline(line, 255);
+    // cout << line << endl;
+
+    if(!fi.good()){
+      break;
+    }
+
+    line_new = TString::Format("%s", line);
+
+    int a = line_new.Last(',');
+    int b = line_new.Length();
+    TString i_str = line_new(0, a);
+    i = i_str.Atoi();
+    TString c_str = line_new(a+1, b);
+    c = c_str.Atoi();
+
+    // cout << i << "  " << c << endl;
+    
+    h->SetBinContent(i+1, c);
+  }
+
+  TFile *file_out = new TFile(TString::Format("%s_%skeV.root", target.Data(), energy.Data()).Data(), "recreate");
+  file_out->cd();
+  h->Write();
+  tr->Write();
+
+  delete h;
+  file_out->Close();
+  fi.close();
+
+  cout << endl;
+}
