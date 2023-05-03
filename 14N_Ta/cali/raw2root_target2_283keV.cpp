fc19efdc127e626ca54b6f6b8ed27b63f917cb76
hanX3 <hanxingchi@gmail.com>
2023-05-03 11:17:49 +0800

    add cali

---
+++ 14N_Ta/cali/raw2root_target2_283keV.cpp
@@ -0,0 +1,33 @@
+
+#define p0 -15.0834
+#define p1 0.626528 
+
+void raw2root_target2_283keV()
+{
+  TFile *fi = TFile::Open("../../../data/14N_Ta/target2/283keV/target2_283keV.root");
+  if(fi->IsZombie()){
+    cout << "wrong open the file" << endl;
+    return;
+  }
+
+  TTree *tr = (TTree*)fi->Get("tr");
+
+  double a = p0;
+  double b = p0+16384*p1;
+  cout << a << " " << b << endl;
+
+  TH1D *h_cali = new TH1D("h_cali", "", 16384, a, b);
+  TString leaf1 = TString::Format("%f+%lf*e_trapz[0]>>h_cali",p0,p1);
+  TString leaf2 = TString::Format("hit_num_trapz==1 && e_trapz[0]>0");
+  cout << leaf1 << endl;
+  cout << leaf2 << endl;
+
+  tr->Draw(leaf1.Data(), leaf2.Data());
+
+  TFile *fo = new TFile("rootfile/target2_283keV_cali.root", "recreate");
+  fo->cd();
+  h_cali->Write();
+
+  fi->Close();
+  fo->Close();
+}
